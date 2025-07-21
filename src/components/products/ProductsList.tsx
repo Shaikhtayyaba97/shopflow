ca91a97eea43fc9a14b72715f081b035aa5cf6b8
Tayyaba Shahbaz <tayyabashahbaz2020@gmail.com>
2025-07-21 14:11:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/products/ProductsList.tsx
+++ src/components/products/ProductsList.tsx
@@ -25,6 +25,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { ProductForm } from './ProductForm';
 import { useToast } from '@/hooks/use-toast';
